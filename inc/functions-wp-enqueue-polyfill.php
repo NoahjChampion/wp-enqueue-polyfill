541218ced61274f7f8ba941fd4419caaa771d2e3
Kyle Reicks <reicks.kyle@gmail.com>
2014-02-08 15:33:44 -0600

    Add in a default src attribute

--- inc/functions-wp-enqueue-polyfill.php
+++ inc/functions-wp-enqueue-polyfill.php
@@ -4,6 +4,6 @@ function wp_register_polyfill($handle, $src, $condition = 'IE', $version = false
   WP_Enqueue_Polyfill::get_instance()->register_polyfill($handle, $src, $condition, $version);
 }
 
-function wp_enqueue_polyfill($handle, $src, $condition = 'IE', $version = false){
+function wp_enqueue_polyfill($handle, $src = '', $condition = 'IE', $version = false){
   WP_Enqueue_Polyfill::get_instance()->enqueue_polyfill($handle, $src, $condition, $version);
 }
